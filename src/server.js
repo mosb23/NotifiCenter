1857389e187bf8d744e1f4ffe119bcaafa3fce52
mosb23 <108569283+mosb23@users.noreply.github.com>
2025-07-21 13:58:54 +0300

    changes

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true,
 }));
 

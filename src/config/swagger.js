ddebfba7b15cfb851933f784c635e3a602b3d28c
mosb23 <108569283+mosb23@users.noreply.github.com>
2025-07-21 13:49:01 +0300

    changes

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/api',
+        url: 'http://https://notificenter-production.up.railway.app/',
         description: 'Development server',
       },
     ],
